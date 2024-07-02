fa8cf201426e5c5b578e9ed59df69296ae66eec8
CaitlynStocker <caitlyn.stocker@octopus.com>
2024-07-02 10:21:55 +1000

    Start up

---
+++ Sparklarkle/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+namespace Sparklarkle;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        StringInput input = new StringInput();
+        int num = input.GetInput();
+        
+        Console.OutputEncoding = Encoding.UTF8;
+        string sparkleString = "\u2728 ";
+
+        Enumerable.Range(0, num).ToList().ForEach(_ => Console.Write(sparkleString));
+    }
+}
+
+public interface IInput
+{
+    int GetInput();
+}
+
+public class StringInput: IInput
+{
+    public int GetInput()
+    {
+        if (int.TryParse(Console.ReadLine(), out int result)) return result;
+
+        return 0;
+    }
+}
