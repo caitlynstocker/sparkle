d39b65522e66b0c492bc258c386a3f2720c5827b
CaitlynStocker <caitlyn.stocker@octopus.com>
2024-07-02 11:37:09 +1000

    Dump in a test written by copilot

---
+++ Sparklarkle.Tests/Sparklarkle.Tests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Sparklarkle.Tests;
+
+public class UnnecessaryClientClassTests
+{
+    [Theory]
+    [InlineData("3", "\u2728 \u2728 \u2728 \r\n")]
+    [InlineData("cats", "cats\u2728 \r\n")]
+    [InlineData("5000", "Really? Why so many??\u2728 \r\n")]
+    public void Run_HandlesVariousInputs_CorrectlyPrintsOutput(string input, string expectedOutput)
+    {
+        // Arrange
+        var client = new UnnecessaryClientClass(input);
+
+        // Redirect console output
+        using (var sw = new StringWriter())
+        {
+            Console.SetOut(sw);
+
+            // Act
+            client.Run();
+
+            // Assert
+            var result = sw.ToString();
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+}
--- Sparklarkle/Program.cs
+++ Sparklarkle/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text;
+﻿using System.Text;
 
 namespace Sparklarkle;
 
@@ -8,36 +6,77 @@ public class Program
 {
     static void Main(string[] args)
     {
-        StringInput input = new StringInput();
-        int num = input.GetInput();
-        
-        PrintSparkles print = new PrintSparkles();
-        print.Print(num);
+        Console.WriteLine("Want some sparkles?");
+        string input;
+        do
+        {
+            input = Console.ReadLine() ?? "";
+            UnnecessaryClientClass client = new UnnecessaryClientClass(input);
+            client.Run();
+        } while (input != "exit");
     }
 }
 
-public interface IInput
+public class InputChecker(string input)
 {
-    int GetInput();
-}
-
-public class StringInput: IInput
-{
-    public int GetInput()
+    public int Check()
     {
-        if (int.TryParse(Console.ReadLine(), out int result)) return result;
+        if (int.TryParse(input, out int result)) return result;
 
         return 0;
     }
 }
 
-public class PrintSparkles()
+public interface IPrint
 {
-    public void Print(int num)
+    void Print();
+}
+
+public class PrintSparkles(int num): IPrint
+{
+    public void Print()
     {
         Console.OutputEncoding = Encoding.UTF8;
         string sparkleString = "\u2728 ";
 
         Enumerable.Range(0, num).ToList().ForEach(_ => Console.Write(sparkleString));
+        Console.WriteLine();
+    }
+}
+
+public class PrintText(string str): IPrint
+{
+    public void Print()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        string sparkleString = "\u2728 ";
+
+        Console.WriteLine(str + sparkleString);
+    }
+}
+
+public class UnnecessaryClientClass(string input)
+{
+    public void Run()
+    {
+        InputChecker checkInput = new InputChecker(input);
+        int num = checkInput.Check();
+
+        if (num == 0)
+        {
+            string text = input.ToString() ?? "Sparkle";
+            PrintText printText = new PrintText(text); 
+            printText.Print();
+        }
+        else if (num > 1000)
+        {
+            PrintText printText = new PrintText("Really? Why so many??"); 
+            printText.Print();
+        }
+        else
+        {
+            PrintSparkles printSparkles = new PrintSparkles(num);
+            printSparkles.Print();
+        }   
     }
 }
